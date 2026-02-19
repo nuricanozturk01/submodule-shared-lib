1aa637340de3c3408fd38e5564d0868f2a30fe30
Nuri Can ÖZTÜRK <nuricanozturk01@gmail.com>
2026-02-19 15:28:02 +0300

    initial: shared-lib

---
+++ src/main/java/testapp/sharedlib/HelloService.java
@@ -0,0 +1,14 @@
+package testapp.sharedlib;
+
+
+import org.jspecify.annotations.NonNull;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+  public @NonNull String hello() {
+
+    return "Hello";
+  }
+}
---
+++ src/test/java/testapp/sharedlib/SharedLibApplicationTests.java
@@ -0,0 +1,13 @@
+package testapp.sharedlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedLibApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
